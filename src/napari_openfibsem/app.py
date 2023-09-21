586533de6148ff3c0a6c8453f31e01ec2483ec82
Daviddierickx <David.Dierickx1@monash.edu>
2023-09-21 18:17:16 +1000

    initial commit

---
+++ src/napari_openfibsem/app.py
@@ -0,0 +1,7 @@
+
+
+from autolamella.ui.AutoLamellaUI import AutoLamellaUI
+from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
+autolamella_ui = AutoLamellaUI
+autoliftout_ui = AutoLiftoutUIv2
+
