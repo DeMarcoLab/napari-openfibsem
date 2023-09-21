bd61186e16b0e7feb9b0329759766e2d78400826
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-09-21 18:49:50 +1000

    add fibsem ui to plugin menu

--- src/napari_openfibsem/app.py
+++ src/napari_openfibsem/app.py
@@ -1,7 +1,9 @@
 
-
+from fibsem.ui.FibsemUI import FibsemUI
 from autolamella.ui.AutoLamellaUI import AutoLamellaUI
 from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
+
+fibsem_ui = FibsemUI
 autolamella_ui = AutoLamellaUI
 autoliftout_ui = AutoLiftoutUIv2
 
