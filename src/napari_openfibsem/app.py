962288d446a9e3086503851ef76fdfe5a659be78
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-10-10 14:38:13 +1100

    add labelling ui as widget

--- src/napari_openfibsem/app.py
+++ src/napari_openfibsem/app.py
@@ -1,9 +1,11 @@
 
 from fibsem.ui.FibsemUI import FibsemUI
+from fibsem.ui.FibsemLabellingUI import FibsemLabellingUI
 from autolamella.ui.AutoLamellaUI import AutoLamellaUI
 from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
 
 fibsem_ui = FibsemUI
+fibsem_labelling_ui = FibsemLabellingUI
 autolamella_ui = AutoLamellaUI
 autoliftout_ui = AutoLiftoutUIv2
 
