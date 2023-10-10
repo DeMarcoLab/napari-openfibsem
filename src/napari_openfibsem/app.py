b90e81b71d3c797b2d1fee8b289b35fad0a541a8
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-10-10 16:31:48 +1100

    add image viewer plugin widget

--- src/napari_openfibsem/app.py
+++ src/napari_openfibsem/app.py
@@ -1,10 +1,12 @@
 
 from fibsem.ui.FibsemUI import FibsemUI
 from fibsem.ui.FibsemLabellingUI import FibsemLabellingUI
+from fibsem.ui.FibsemImageViewer import FibsemImageViewer
 from autolamella.ui.AutoLamellaUI import AutoLamellaUI
 from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
 
 fibsem_ui = FibsemUI
+fibsem_image_viewer = FibsemImageViewer
 fibsem_labelling_ui = FibsemLabellingUI
 autolamella_ui = AutoLamellaUI
 autoliftout_ui = AutoLiftoutUIv2
