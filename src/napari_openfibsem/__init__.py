e0a74848b3f897a9b592ac7ab109186d383cecdc
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-10-10 16:33:45 +1100

    fix: import

--- src/napari_openfibsem/__init__.py
+++ src/napari_openfibsem/__init__.py
@@ -4,9 +4,12 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from .app import autolamella_ui, autoliftout_ui
+from .app import autolamella_ui, autoliftout_ui, fibsem_ui, fibsem_image_viewer, fibsem_labelling_ui
 
 __all__ = (
+    "fibsem_ui",
+    "fibsem_image_viewer",
+    "fibsem_labelling_ui",
     "autolamella_ui",
     "autoliftout_ui",
 )
