9cfc90299d11a06a71c52d2adb98566edd6ade07
Patrick Cleeve <pcleeve@gmail.com>
2023-12-23 20:53:22 +1100

    add feature detection plugin

--- src/napari_openfibsem/app.py
+++ src/napari_openfibsem/app.py
@@ -1,6 +1,7 @@
 
 from fibsem.ui.FibsemUI import FibsemUI
 from fibsem.ui.FibsemLabellingUI import FibsemLabellingUI
+from fibsem.ui.FibsemFeatureDetectionUI import FibsemFeatureDetectionUI
 from fibsem.ui.FibsemImageViewer import FibsemImageViewer
 from autolamella.ui.AutoLamellaUI import AutoLamellaUI
 from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
@@ -8,6 +9,7 @@ from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
 fibsem_ui = FibsemUI
 fibsem_image_viewer = FibsemImageViewer
 fibsem_labelling_ui = FibsemLabellingUI
+fibsem_feature_detection_ui = FibsemFeatureDetectionUI
 autolamella_ui = AutoLamellaUI
 autoliftout_ui = AutoLiftoutUIv2
 
