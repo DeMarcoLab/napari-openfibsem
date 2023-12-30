a289b0d978f6fa2a95efc8c4e6013f4ba11196da
Patrick Cleeve <pcleeve@gmail.com>
2023-12-30 19:57:04 +1100

    mv feature labelling

--- src/napari_openfibsem/app.py
+++ src/napari_openfibsem/app.py
@@ -1,7 +1,7 @@
 
 from fibsem.ui.FibsemUI import FibsemUI
 from fibsem.ui.FibsemLabellingUI import FibsemLabellingUI
-from fibsem.ui.FibsemFeatureDetectionUI import FibsemFeatureDetectionUI
+from fibsem.ui.FibsemFeatureLabellingUI import FibsemFeatureLabellingUI
 from fibsem.ui.FibsemImageViewer import FibsemImageViewer
 from autolamella.ui.AutoLamellaUI import AutoLamellaUI
 from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
@@ -9,7 +9,7 @@ from autolamella.liftout.ui.AutoLiftoutUIv2 import AutoLiftoutUIv2
 fibsem_ui = FibsemUI
 fibsem_image_viewer = FibsemImageViewer
 fibsem_labelling_ui = FibsemLabellingUI
-fibsem_feature_detection_ui = FibsemFeatureDetectionUI
+fibsem_feature_labelling_ui = FibsemFeatureLabellingUI
 autolamella_ui = AutoLamellaUI
 autoliftout_ui = AutoLiftoutUIv2
 
